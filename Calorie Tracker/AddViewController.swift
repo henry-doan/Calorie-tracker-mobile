761f10ab9983ad3141dc0b896e0cc254799492ce
Henry Doan <henrydoan96@gmail.com>
2021-07-30 14:58:23 -0600

    initial views

---
+++ Calorie Tracker/AddViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddViewController.swift
+//  Calorie Tracker
+//
+//  Created by Henry Doan on 7/30/21.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
